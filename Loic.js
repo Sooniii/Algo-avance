f33af70e6fb95fcf237f02b17e2e81af4a78e4ec
Loic-Gouriou <loicgouriou6@gmail.com>
2021-02-03 18:27:59 +0100

    story 3 tri date non fini

--- Loic.js
+++ Loic.js
@@ -1,1 +1,24 @@
-console.log('helloword')
+//node Loic.js –action sort_date ./movies.json ./movies.sort.date.json
+
+let tab = process.argv
+
+const fs = require("fs");
+data = fs.readFileSync(tab[4],"utf8");
+
+let json = JSON.parse(data);
+
+json.sort(tri);
+
+let jsonOut = JSON.stringify(json,null,"\t")
+fs.writeFileSync(tab[5],jsonOut);
+
+
+
+function tri(a,b){
+    if (a.release_date<b.release_date) return -1;
+    else if (a.release_date == b.release_date) return 0;
+    else return 1;
+}
+
+
+
