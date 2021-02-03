5fc8c6a60416fe79c1f63543dc7c0d0b178c8332
Sooniii <theodulejanchen@hotmail.fr>
2021-02-03 15:25:30 +0100

    Story 2

--- Projet.js
+++ Projet.js
@@ -1,9 +1,15 @@
-//node Projet -action transform ./movies.json ./movies.out.json
+//node Projet.js -action transform ./movies.json ./movies.out.json
 
-let tab = process.argv
+let tab = process.argv;
+let beginTime = new Date;
 
 const fs = require("fs");
 data = fs.readFileSync(tab[4],"utf8");
 
 let stringOut = "test";
 fs.writeFileSync(tab[5],stringOut);
+
+let endTime = new Date;
+let appTime = endTime - beginTime;
+
+console.log("L'appli a mis " + appTime + "ms pour être executé.");
