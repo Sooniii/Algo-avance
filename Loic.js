cf432d739e955e04b3ae702ffb97c3ed1bba076e
Loic-Gouriou <loicgouriou6@gmail.com>
2021-02-04 14:44:38 +0100

    Ajout de la fonction de tri, pas encore fini

--- Loic.js
+++ Loic.js
@@ -7,18 +7,42 @@ data = fs.readFileSync(tab[4],"utf8");
 
 let json = JSON.parse(data);
 
-json.sort(tri);
+    function swap(json,a,b){
+    let temp = json[a];
+    json[a] = json[b]
+    json[b] = temp
+}
+
+    function sort(json){
+    for(let i = json.length-1; i>=1 ;i--){
+        for(let j = 0; j<=i-1;j++){
+            if(json[j+1] < json[j]){
+                swap(json,j+1,j);
+            }
+        }
+    }
+}
+
+function tri(a,b)
+{
+	if (a < b) return -1;
+	else if (a == b) return 0;
+	else return 1;
+}
+
+console.log('tri en cours')
+tri (json);
+sort(json);
+console.log('tri terminé')
 
 let jsonOut = JSON.stringify(json,null,"\t")
 fs.writeFileSync(tab[5],jsonOut);
 
+console.log('cest fait')
 
 
-function tri(a,b){
-    if (a.release_date<b.release_date) return -1;
-    else if (a.release_date == b.release_date) return 0;
-    else return 1;
-}
+
+
 
 
 
