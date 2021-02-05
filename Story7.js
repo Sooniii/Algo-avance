3cea7799e7685ab688f3dfc980a8bd2374b9b1d1
Loic-Gouriou <loicgouriou6@gmail.com>
2021-02-05 15:46:49 +0100

    Vraiment fini

--- Story7.js
+++ Story7.js
@@ -1,13 +1,13 @@
 
-//node Loic.js –action color <path_folder>»
+//node Story7.js –action color <path_folder>»
 let tab = process.argv;
 
 
 const jimp = require('jimp')
 const fs = require("fs");
 let tabFiles = fs.readdirSync(tab[4])
-let moyenne
 
+//Boucle qui permet de regarder la couleur de chaque image d'un fichier
 for (i = 0; i < tabFiles.length; i++){
     let path_image = ToPath(tab[4], tabFiles[i])
     let promise = jimp.read(path_image);
@@ -16,13 +16,15 @@ for (i = 0; i < tabFiles.length; i++){
         let y = 100;
         let hex = image.getPixelColor(x, y);
         // returns the colour of that pixel e.g. 0xFFFFFFFF
-        jimp.intToRGBA(hex);
+        let rgb = jimp.intToRGBA(hex);
         // e.g. converts 0xFFFFFFFF to {r: 255, g: 255, b: 255, a:255}
+        console.log(rgb)
 
             });
 }
 
 
+//fonction qui permet de retourner le path complet
 function ToPath(path_folder, imageName) {
 //path_folder peut comporter des strings (ex :./image/)
 //imageName peut comporter (ex : AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg)
